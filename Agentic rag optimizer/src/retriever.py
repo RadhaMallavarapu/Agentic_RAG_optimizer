bdb1fd960b8a77ed6643cdba7ec23e6c6898229e
Radha Mallavarapu <RadhaMallavarapuai01@gmail.com>
2026-03-21 18:46:12 +0530

    Update retriever.py

--- Agentic rag optimizer/src/retriever.py
+++ Agentic rag optimizer/src/retriever.py
@@ -1,1 +1,26 @@
+# Retriever.py
+# Handles embeddings and vector store (FAISS)
 
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+
+def create_embeddings():
+  """ 
+  creating embedding model
+  """
+  embeddings = HuggingFaceEmbeddings()
+  return embeddings
+
+def create_vector_store(chunks,embeddings):
+  """
+  Store document chunks in FAISS vector database.
+  """
+  vectorstore = FAISS.from_documents(chunks, embeddings)
+  return vectorstore
+
+def get_retriever(vectorstore):
+  """
+  create retriever from vector store.
+  """
+  retriever = vectorstore.as_retriever()
+  return retriever
